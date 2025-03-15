c3b57b05cf424bbeba427ccf1b0f49d5a7dc2073
StepfenShawn <m18824909883@163.com>
2025-03-15 17:14:05 +0800

    bug: fix vite.config.ts

--- dist/assets/javascript-283db239.js
+++ dist/assets/javascript-283db239.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-1978697e.js";import"./index-28cf4f10.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-c391602c.js";import"./index-9805f594.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.47.0(69991d66135e4a1fc1cf0b1ac4ad25d429866a0d)
  * Released under the MIT license
